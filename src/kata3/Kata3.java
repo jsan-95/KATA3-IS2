ed746991b51a7c4c26326ebaf02a386cae7a048f
jsan-95 <jesus.teror@hotmail.com>
2017-11-03 08:45:58 +0000

    Add files via upload

--- src/kata3/Kata3.java
+++ src/kata3/Kata3.java
@@ -5,17 +5,16 @@ public class Kata3 {
     
     public static void main(String[] args) {
         Histogram histogram = new Histogram<>();
-        histogram.increment("gmail.com");
-        histogram.increment("gmail.com");
-        histogram.increment("ulpgc.com");
-        histogram.increment("gmail.com");
-        histogram.increment("outlook.com");
-        histogram.increment("ulpgc.com");
-        histogram.increment("ull.com");
-        histogram.increment("gmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("hotmail.com");
-        histogram.increment("gmail.com");
+        histogram.increment(3,8);        
+        histogram.increment(2,1);
+        histogram.increment(2,2);
+        histogram.increment(3,4);
+        histogram.increment(1,2);
+        histogram.increment("ull.com",4);
+        histogram.increment("gmail.com",5);
+        histogram.increment("hotmail.com",2);
+        histogram.increment("hotmail.com",1);
+        histogram.increment("gmail.com",6);
         
         new HistogramDisplay(histogram).execute();
     }
